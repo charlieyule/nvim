0789d9f91d396cca9034b862392f1c4d59f491ec
Charlie <kntrevita@163.com>
2023-01-14 13:51:24 +0800

    Add basic nvim options

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require("options")
+
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+-- :help options
+local o = vim.opt
+
+o.cursorline = true
+o.expandtab = true
+o.mouse = "a"
+o.number = true
+o.shiftwidth = 2
+o.showtabline = 2
+o.splitbelow = true
+o.splitright = true
+o.wrap = false
+o.iskeyword:append "-"
