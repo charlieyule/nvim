cc9dc5322802b27d96dd4af08de57c2d4a1a501e
Charlie <kntrevita@163.com>
2023-01-14 14:32:14 +0800

    Add keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = ";"
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 20<CR>", opts)
