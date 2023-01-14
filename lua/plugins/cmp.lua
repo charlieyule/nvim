6f42ecfb70492768a9b626ac85821f7e84110222
Charlie <kntrevita@163.com>
2023-01-14 23:58:44 +0800

    Install cmp plugins

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,7 @@
+return {
+  { "hrsh7th/cmp-nvim-lsp", lazy = false },
+  { "hrsh7th/cmp-buffer", lazy = false },
+  { "hrsh7th/cmp-path", lazy = false },
+  { "hrsh7th/cmp-cmdline", lazy = false },
+  { "hrsh7th/nvim-cmp", lazy = false },
+}
