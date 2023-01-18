aad622f04046b557f69755b59a5b37974cb61264
Charlie <kntrevita@163.com>
2023-01-18 23:35:29 +0800

    Hide vim-tree root folder

--- lua/setups/explorer.lua
+++ lua/setups/explorer.lua
@@ -6,6 +6,7 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 require("nvim-tree").setup {
   disable_netrw = false,
   view = {
+    hide_root_folder = true,
     mappings = {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
