e9e1298f85ea4f71ca13b4b999e456201a98df5a
Charlie <kntrevita@163.com>
2023-01-20 01:24:33 +0800

    Use gofumpt and staticcheck for Go

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,14 +4,19 @@ return {
     config = function()
       local null_ls = require("null-ls")
       local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
       require("null-ls").setup({
         sources = {
+          -- Lua
           formatting.stylua.with({
             extra_args = {
               "--indent-type=Spaces",
               "--indent-width=2",
             },
           }),
+          -- Go
+          formatting.gofumpt,
+          diagnostics.staticcheck,
         },
       })
     end,
