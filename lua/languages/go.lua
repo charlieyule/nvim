30c7bf94b216003fccfc1b1d32554458db8e6fa4
Charlie <kntrevita@163.com>
2023-01-31 21:21:24 +0800

    [Go] Replace staticcheck with golangci-lint

--- lua/languages/go.lua
+++ lua/languages/go.lua
@@ -2,5 +2,5 @@ local lang_global = require("languages.global")
 
 lang_global.add_language_server("gopls")
 lang_global.add_null_ls_source(lang_global.FORMATTING, "gofumpt")
-lang_global.add_null_ls_source(lang_global.DIAGNOSTICS, "staticcheck")
+lang_global.add_null_ls_source(lang_global.DIAGNOSTICS, "golangci_lint")
 lang_global.add_telescope_ingore_pattern("vendor")
